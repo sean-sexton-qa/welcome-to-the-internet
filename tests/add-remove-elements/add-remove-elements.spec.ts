0f0ec522f1f079f3cd59c2e5a31a3cdc93868e8f
Sean Sexton <seansextonqa@gmail.com>
2025-12-08 22:07:40 +0100

    test: add check that Delete button is added when clicking Add Element

--- tests/add-remove-elements/add-remove-elements.spec.ts
+++ tests/add-remove-elements/add-remove-elements.spec.ts
@@ -6,7 +6,16 @@ test('should navigate to the Add/Remove Elements page', async ({ page }) => {
   //When
   await page.getByRole('link', { name: 'Add/Remove Elements' }).click();
   //Then
-  await expect(page).toHaveURL('/add_remove_elements/')
+  await expect(page).toHaveURL('/add_remove_elements/');
   await expect(page.getByRole('heading', { level: 3 }))
     .toHaveText("Add/Remove Elements");
+});
+
+test('should add a new delete button when clicking "Add Element"', async ({ page }) => {
+  //Given
+  await page.goto('/add_remove_elements/');
+  //When
+  await page.getByRole('button', { name: 'Add Element'}).click();
+  //Then
+  await expect(page.getByRole('button', { name: 'Delete'})).toBeVisible();
 });
